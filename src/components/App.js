2976cabd341faad0229e04121a9eff2c6f32eefa
kvndai <kevin.t.dai@gmail.com>
2017-09-26 13:37:57 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from '../assets/logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>SuperSquad</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
