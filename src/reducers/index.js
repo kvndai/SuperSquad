b57ab8569111b42efa9d4da5cdf50ae1d270be02
kvndai <kevin.t.dai@gmail.com>
2017-09-26 13:46:38 -0700

    adding reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import characters_json from '../data/characters.json';
+
+const characters = (state = characters_json, action) => {
+  switch (action.type) {
+    case ACTION_TYPE_1:
+      return state
+    case ACTION_TYPE_2:
+      return state
+    default:
+      return state
+  }
+}
+
+export default characters;
